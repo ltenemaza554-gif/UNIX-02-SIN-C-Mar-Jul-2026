13668bd286dc1161019f0e748975f12ec54f3a29
ltenemaza554-gif <ltenemaza554@gmail.com>
2026-03-27 03:22:31 +0000

    Step 8: Manage trust and sign keys

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,4 +1,4 @@
-# Step 2: Generate GPG key pair
+# Step 2: Generate GPG key pa
 # Step 2: Generate GPG key pair
 # Step 3: List GPG keys
 # Step 4: Export and import public keys
@@ -6,3 +6,5 @@
 # Step 6: Encrypt and decrypt file
 # Step 7: Sign and verify file
 # Step 9: Encrypt and sign final file
+# Step 8: Manage trust and sign keys
+# Step 8: Manage trust and sign keys
