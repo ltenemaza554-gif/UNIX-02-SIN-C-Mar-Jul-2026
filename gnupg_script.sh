a04cf663fa3238eb5d2aeaa456d0c92079b78364
ltenemaza554-gif <ltenemaza554@gmail.com>
2026-04-14 14:18:50 +0000

    sudo

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,10 +1,2 @@
-# Step 2: Generate GPG key pa
-# Step 2: Generate GPG key pair
-# Step 3: List GPG keys
-# Step 4: Export and import public keys
-# Step 5: Create plain text file
-# Step 6: Encrypt and decrypt file
-# Step 7: Sign and verify file
-# Step 9: Encrypt and sign final file
-# Step 8: Manage trust and sign keys
-# Step 8: Manage trust and sign keys
+sudo apt update
+#refresh the list of available packages.
