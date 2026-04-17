00430f36479df30d1bffe3e8c449a60586ad227d
ltenemaza554-gif <ltenemaza554@gmail.com>
2026-04-17 15:56:38 +0000

    recuperacion

--- recuperacion.sh
+++ recuperacion.sh
@@ -1,8 +1,13 @@
 # crear una carprta
 mkdir nueva carpeta 
 #sirve para crear un archio
-touch nuevo archivo
+touch  nuevo archivo
 #compruebo que se lista los archivos y directorios 
 ls 
-#
-
+cd 
+#Sirve para moverte entre carpetas dentro del sistema de archivos desde la terminal.
+cd nueva carpeta
+cat 
+#me sirve para pasar de una carpeta hacia otra carpeta 
+cd..
+#me funciona para salirme del 
