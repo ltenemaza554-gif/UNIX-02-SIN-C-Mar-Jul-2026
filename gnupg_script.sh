883e9e1e28b5b9a8f33b9bc39a72d4e1a80160b5
ltenemaza554-gif <ltenemaza554@gmail.com>
2026-06-01 12:36:30 +0000

    1

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,10 +1,11 @@
-# Step 2: Generate GPG key pa
-# Step 2: Generate GPG key pair
-# Step 3: List GPG keys
-# Step 4: Export and import public keys
-# Step 5: Create plain text file
-# Step 6: Encrypt and decrypt file
-# Step 7: Sign and verify file
-# Step 9: Encrypt and sign final file
-# Step 8: Manage trust and sign keys
-# Step 8: Manage trust and sign keys
+id
+uid=1000(codespace) gid=1000(codespace) groups=1000(codespace),985(pipx),986(python),987(oryx),988(golang),989(docker),990(sdkman),991(rvm),992(php),993(conda),994(nvs),995(nvm),996(hugo),1001(ssh)
+id -gn
+codespace
+touch ~/test_grupo_heredado.txt
+ls -la ~/test_grupo_heredado.txt
+-rw-r--r-- 1 codespace codespace 0 Jun  1 12:34 /home/codespace/test_grupo_heredado.txt
+id -gn
+echo "Grupo actual: $(id -gn)"
+Grupo actual: codespace
+touch ~/antes_de_newgrp.txt
