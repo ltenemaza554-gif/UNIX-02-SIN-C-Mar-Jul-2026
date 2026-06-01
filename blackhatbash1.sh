b92c1774b25f0ad906d08ec96158967ab10a169e
ltenemaza554-gif <ltenemaza554@gmail.com>
2026-06-01 15:20:45 +0000

    commit 3

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,6 +1,23 @@
+#!/bin/bash
+set -x
 bash --version 
 env
 /bin/bash
 echo ${SHELL}
 echo ${RANDOM}
 echo ${OSTYPE}
+ps -ef
+ps -e -f
+df --human-readable
+#!/bin/bash
+#!/usr/bin/env bash
+#bash -balckhatbash1.sh
+#!/bin/bash -r
+#
+#!/bin/bash -x
+#
+#script.sh
+#bash script.sh sh
+set +x
+
+bash -blackhatbash1.sh
