b2aa27dc338320ad6e084e427ee1eb17873ec824
ltenemaza554-gif <ltenemaza554@gmail.com>
2026-06-05 15:38:05 +0000

    lzl

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -6,3 +6,19 @@ ls; ps; whoami
 
 lzl || echo "El comando lzl fallo "
 
+echo "Hello World!" > output.txt
+cat output.txt
+
+echo "Goodbye!" > output.txt
+cat output.txt
+
+echo "Goodbye!" >> output.txt
+cat output.txt
+
+ls -l / &> stdout_and_stderr.txt
+
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stderr.txt
+
+ls -l / &>> stdout_and_stderr.txt
+
