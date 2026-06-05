8f4d6acc6945fa0022c5874190d0fd2fc4038cfd
ltenemaza554-gif <ltenemaza554@gmail.com>
2026-06-05 14:18:51 +0000

    1

--- local_scope_variable.sh
+++ local_scope_variable.sh
@@ -12,6 +12,8 @@ print_name
 
 echo "Variable ${name} will not be printed because it is a l
 ocal variable."
-#cual es la diferencia entre una variable local y global y que particularidad en la invocacion de funciones en bash
-#las variables globales son accesibles desde cualquier parte del script, mientras que las variables locales están restringidas únicamente a la función donde se declaran.
-#En Bash, todas las variables son globales por defecto, incluso si se definen dentro de una función.
+
+# What is the difference between a local and a global variable, and what is the particularity of function invocation in Bash?
+# Global variables are accessible from anywhere in the script, while local variables are restricted only to the function where they are declared.
+# In Bash, all variables are global by default, even if they are defined inside a function.
+
