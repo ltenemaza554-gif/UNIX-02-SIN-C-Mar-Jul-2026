33747c18b66cbd1fb7b20e722e6e35a42d1c868b
ltenemaza554-gif <ltenemaza554@gmail.com>
2026-06-08 15:32:42 +0000

    commitname

---
+++ input _prompting.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "What is your first name?"
+read -r firstname
+echo "What is your last name?"
+read -r lastname
+echo "Your first name is ${firstname} and your last name is
+${lastname}"
