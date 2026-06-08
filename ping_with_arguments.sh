5678ecc3d5914617521c01e1c58b6314b703ff64
ltenemaza554-gif <ltenemaza554@gmail.com>
2026-06-08 15:07:08 +0000

    commitping

---
+++ ping_with_arguments.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+SCRIPT_NAME="${0}"
+TARGET="${1}"
+echo "Running the script ${SCRIPT_NAME}"
+echo "Pinging the target: ${TARGET}..."
+ping -c3 "${TARGET}"
+bash ping_with_arguments.sh 127.0.0.1
