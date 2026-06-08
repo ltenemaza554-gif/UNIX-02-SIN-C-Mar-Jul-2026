5c0cbbc0e9c2a11d4a5f8f600f5133de6f77c269
ltenemaza554-gif <ltenemaza554@gmail.com>
2026-06-08 15:53:06 +0000

    exercise1

---
+++ exercice1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#!/bin/bash
+
+first_name=$1
+last_name=$2
+
+date +"%d-%m-%Y" > output.txt
+
+echo "$first_name $last_name" >> output.txtlast_name=$2
+
+date +"%d-%m-%Y" > output.txt
+
+echo "$first_name $last_name" >> out
