317128eedb00c78ca3f155e7d8eb666af388f490
ltenemaza554-gif <ltenemaza554@gmail.com>
2026-06-12 13:14:06 +0000

    variable one and 2 arreglado

--- string_comparison.sh
+++ string_comparison.sh
@@ -3,9 +3,8 @@
 VARIABLE_ONE="nostarch"
 VARIABLE_TWO="nostarch"
 
-if [["${VARIABLE_ONE}" == "${VARIABLE_TWO}"]]; then
- echo "They are equal!"
+if [[ "${VARIABLE_ONE}" == "${VARIABLE_TWO}" ]]; then
+    echo "They are equal!"
 else
- echo "They are not equal!"
+    echo "They are not equal!"
 fi
-
