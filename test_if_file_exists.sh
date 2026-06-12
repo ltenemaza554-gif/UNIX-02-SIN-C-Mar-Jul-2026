15ed7b58eec2b339f07b91b4db3af0b74df42d05
ltenemaza554-gif <ltenemaza554@gmail.com>
2026-06-12 12:39:59 +0000

    filename

--- test_if_file_exists.sh
+++ test_if_file_exists.sh
@@ -2,9 +2,17 @@
 
 FILENAME="flow_control_with_if.txt"
 
-if [[ -f "${FILENAME}" ]]; then
-    echo "${FILENAME} already exists."
-    exit 1
+if [[-f "${FILENAME}"]]; then
+ echo "${FILENAME} already exists."
+ exit 1
 else
+ touch "${FILENAME}"
+fi
+#It's the same thing but more concise, so to speak.
+
+if [[ ! -f "${FILENAME}" ]]; then
     touch "${FILENAME}"
 fi
+
+
+
