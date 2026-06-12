e1ed6bfd1fd09f0d46f87f5852b7c9a053e31073
ltenemaza554-gif <ltenemaza554@gmail.com>
2026-06-12 14:35:36 +0000

    death code

--- if_elif.sh
+++ if_elif.sh
@@ -6,6 +6,8 @@ if [[ -z "${USER_INPUT}" ]]; then
     exit 1
 fi
 
+#- Exit early with error if USER_INPUT is not provided
+
 
 if [[ -f "${USER_INPUT}" ]]; then
     echo "${USER_INPUT} is a file."
@@ -14,3 +16,4 @@ elif [[ -d "${USER_INPUT}" ]]; then
 else
     echo "${USER_INPUT} is not a file or a directory."
 fi
+#
